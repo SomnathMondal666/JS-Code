7f366ede3f7587fdd825fd101d2876403700e38e
Somnath Mondal <132666285+SomnathMondal666@users.noreply.github.com>
2024-03-27 20:02:55 +0000

    28-03-2024

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -103,7 +103,254 @@
 // console.log("2" === 2);//false
 // console.log("2" == 2);//true
 
-// Primitive
+// categorization oficially as the based of how to store the data and how to access the data
+// Primitive:
+// 7 types of data: String, number, bollean, null, undefined, Symbol, BigInt
+// const id = Symbol('123')
+// const id1 = Symbol('123')
+
+// console.log(id === id1)//false
 
 
+//Reference (Non primitive)
+//Array, Objects, Functions
+// const heros = ["Shaktiman", "Naagraj", "doga"];
+// let myobj={
+//     name:"hitesh",
+//     age:22
+// }
+
+// const myFunction=function(){
+//     console.log("Hello World");
+// }
+// console.log(typeof myFunction);
+
+/*Memories*/
+//Stack(Primitive) , Heap(non-Primitive)
+// let myYoutube="som"
+// let anotherName=myYoutube
+// anotherName = "Somnath"
+
+// console.log(myYoutube)
+// console.log(anotherName)
+
+// let userone = {
+//     email:"user@google.com"
+// }
+// let usertwo = userone
+
+// usertwo.email = "som@gmail.com"
+
+// console.log(userone.email); //som@gmail.com
+// console.log(usertwo.email); //som@gmail.com
+
+/* String */
+// const myname = "Som"
+// const repocount = 40;
+// const gamaename = new String('Somnath')//another type of declearation if you run in cnsole it's display as object
+// console.log(`Hello my name is ${myname} and my repo count is ${repocount}`) //Hello my name is Som and my repo count is 40
+// console.log(gamaename[1]);
+// console.log(gamaename.length);
+// console.log(myname.toLocaleUpperCase());
+// console.log(myname.charAt(2));
+// console.log(myname.indexOf('m'));
+
+// all string meathods
+/* anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]() */
+
+// const newString = gamaename.substring(0,4)
+// console.log(newString);
+
+// const newString = gamaename.slice(1,4)
+// console.log(newString);
+
+//string.trim(), trimStart(), trimEnd()
+//string.replace('present value wants to change','new value want to replace') 
+//string.split('seperator')
+//string.include('string')
+
+/*Numbers*/
+// const score = 400;
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) //for how many numbers to see after point
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3)); //presized value
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/* Maths */
+// console.log(Math);//object
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.3))//4
+// console.log(Math.ceil(4.3))//5
+// console.log(Math.floor(4.3))//4
+// console.log(Math.max(4,3,2))//4
+// console.log(Math.min(4,3,2))//2
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);//0 not comming
+
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max-min + 1))+min) //formula for getting random value but not getting 0
 
